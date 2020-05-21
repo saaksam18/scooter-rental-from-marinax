2fe08bd7b6a958c020b58995254a633ca0c2f525
marinax-khm-kanha <kanha@marinax.co.jp>
2020-05-21 16:03:59 +0700

    - create layout sidebar 	+ header sidebar 	+ menu 	+ search block

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,6 +11,21 @@ export class SidebarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const $menu = $(".menu_icon");
+		$(document).mouseup(e => {
+			if (!$menu.is(e.target)
+			&& $menu.has(e.target).length === 0)
+			{
+				$menu.removeClass('active');
+				// $("#content_menu").addClass("d-md-none");
+			}
+		});
+		
+
+		$(".menu_icon").click(function(){
+			$(this).toggleClass("active");
+			$("#content_menu").removeClass("d-md-none");
+		});
   }
 
 }
