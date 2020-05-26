76c9215bf722a7110925557eba540508b9627fa8
Sreysros <sreysros@marinax.co.jp>
2020-05-26 09:06:55 +0700

    - add block slide to home page

--- src/assets/js/slide.js
+++ src/assets/js/slide.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
       slidesToShow: 3,
       autoplay: true,
       autoplaySpeed: 2000,
+      prevArrow:"<img class='a-left control-c prev slick-prev' style='position: absolute;z-index: 2;height: 100%;mix-blend-mode: multiply;' src='assets/images/prev-arrow.svg'>",
+      nextArrow:"<img class='a-right control-c next slick-next' style='position: absolute;z-index: 2;right: 0;top: 0;height: 100%;mix-blend-mode: multiply;' src='assets/images/next-arrow.svg'>",
       responsive: [
         {
           breakpoint: 768,
