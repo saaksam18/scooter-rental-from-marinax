61f1e6ac5864981ad9cbf38ef4c71c3eb69bb829
Sreysros <sreysros@marinax.co.jp>
2020-05-26 16:52:20 +0700

    - add block slide and repair responsvie style

--- src/assets/js/slide.js
+++ src/assets/js/slide.js
@@ -5,25 +5,14 @@ $(document).ready(function(){
       slidesToShow: 3,
       autoplay: true,
       autoplaySpeed: 2000,
+      arrows: true,
       prevArrow:"<img class='a-left control-c prev slick-prev' style='position: absolute;z-index: 2;height: 100%;mix-blend-mode: multiply;' src='assets/images/prev-arrow.svg'>",
       nextArrow:"<img class='a-right control-c next slick-next' style='position: absolute;z-index: 2;right: 0;top: 0;height: 100%;mix-blend-mode: multiply;' src='assets/images/next-arrow.svg'>",
       responsive: [
         {
           breakpoint: 768,
           settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 3
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 1
+            centerPadding: '0px',
           }
         }
       ]
