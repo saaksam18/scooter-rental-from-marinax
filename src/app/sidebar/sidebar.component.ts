7b4cab001cf6bb94b094549fc5219af14de34620
marinax-khm-kanha <kanha@marinax.co.jp>
2020-05-28 17:07:44 +0700

    added sub-menu

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import $ from 'jquery';
+import { Component, OnInit } from "@angular/core";
+import $ from "jquery";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.scss"]
 })
 export class SidebarComponent implements OnInit {
 
@@ -15,44 +15,45 @@ export class SidebarComponent implements OnInit {
 		// menu responsive
 		$(".menu-icon").click(function(){
 			$(this).toggleClass("active");
-			$("#content-menu").toggleClass('show');
+			$("#content-menu").toggleClass("show");
 		});
-		$(".sidebar__menu ul li").click(function(){
-			$(this).children().toggleClass('is-active');
+		$(".sidebar__menu ul.parent-menu > li > a").click(function(){
+			$(this).toggleClass("is-active");
+			$(this).next(".sub-menu").toggleClass("is-active");
 		});
 
 		// change font-size
-		$(".text-size .text-size__s").on("click", function(){
-			$(this).addClass('active');
-			$(".text-size .text-size__m").removeClass('active');
-			$('html').css('font-size','62.5%');
+		$(".text-size .text-size__s").click(function(){
+			$(this).addClass("active");
+			$(".text-size .text-size__m").removeClass("active");
+			$("html").css("font-size","62.5%");
 		});
 
-		$(".text-size .text-size__m").on("click", function(){
-			$(this).addClass('active');
-			$(".text-size .text-size__s").removeClass('active');
-			$('html').css('font-size','72%');
+		$(".text-size .text-size__m").click(function(){
+			$(this).addClass("active");
+			$(".text-size .text-size__s").removeClass("active");
+			$("html").css("font-size","72%");
 		});
 
 		// change langauge 
-		$('.text-translate li').on("click", function(){
-			$('.text-translate li').removeClass("active");
+		$(".text-translate li").on("click", function(){
+			$(".text-translate li").removeClass("active");
 			$(this).addClass("active");	
 		});
 
 		// search 
-		$('.search-input').on('keyup keypress', function() {
-			if ($(this).val() != '') {
-				$('.clear-text').removeClass('d-none');
+		$(".search-input").on("keyup keypress", function() {
+			if ($(this).val() != "") {
+				$(".clear-text").removeClass("d-none");
 			}else {
-				$('.clear-text').addClass('d-none');
+				$(".clear-text").addClass("d-none");
 			}
 		});
 
 		// clear text search
-		$('.clear-text').on("click", function(){
+		$(".clear-text").on("click", function(){
 			$(this).addClass("d-none");
-			$('.search-input').val('');
+			$(".search-input").val("");
 		});
   	}
 
