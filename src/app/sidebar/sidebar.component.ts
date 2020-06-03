43c3a809d1ffdbba58afdc57684448c49e835c4a
kanha mx <kanha@marinax.co.jp>
2020-06-03 13:47:41 +0700

    added script for hide show content menu on responsive and pc

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -19,31 +19,44 @@ export class SidebarComponent implements OnInit {
 			$(this).toggleClass("active");
 			$("#content-menu").toggleClass("show");
 		});
-		$(".sidebar__menu ul.parent-menu > li > a").click(function(){
-			$(this).toggleClass("is-active");
-			$(this).next(".sub-menu").toggleClass("is-active");
+		$(".sidebar__menu ul.parent-menu > li").click(function(){
+			$(this).find(">a:first-child").toggleClass("is-active");
+			$(this).children(".sub-menu").toggleClass("is-active");
+		});
+
+		$("ul.parent-menu > li ").each(function(){
+			$(this).mouseenter(function(){
+				$(this).children(".sub-menu").css("visibility","visible");
+			});
+
+			$(this).mouseleave(function(){
+				$(this).children(".sub-menu").css("visibility","hidden");
+			});
+		});
+		$(".sub-menu ul li").click(function(){
+			$(".sub-menu").css("visibility","hidden");
 		});
 		
-		$(".parent-menu li").hover(function(){
-			$(this).next(".sub-menu").css("visibility","visible");
-		});
 		if( $( window ).width() <= 768 ) {
 			$(".sub-menu ul li").click(function(){
 				$("#content-menu").removeClass("show");
 				$(".menu-icon").removeClass("active");
 			});
 			
-			$(".parent-menu li a").click(function(){
-				if ( $(this).next(".sub-menu").length == 0 ){
-					$("#content-menu").removeClass("show");
-					$(".menu-icon").removeClass("active");
-					$("ul.parent-menu > li > a").removeClass("is-active");
-					$(".parent-menu").find(".sub-menu").removeClass("is-active");
+			$("ul.parent-menu > li ").each(function(){
+				if($(this).find(".sub-menu").length > 0){
+					$(this).find(">a:first-child").css("pointer-events","none");
 				}else {
-					return false;
+					$(this).click(function(){
+						$("#content-menu").removeClass("show");
+						$(".menu-icon").removeClass("active");
+						$("ul.parent-menu > li").find(".is-active").removeClass("is-active");
+						$(this).next(".sub-menu").removeClass("is-active");
+					});
 				}
 			});
 		}
+
 		// change font-size
 		$(".text-size .text-size__s").click(function(){
 			$(this).addClass("active");
@@ -55,11 +68,13 @@ export class SidebarComponent implements OnInit {
 			$(".text-size .text-size__s").removeClass("active");
 			$("html").css("font-size","72%");
 		});
+
 		// change langauge 
 		$(".text-translate li").on("click", function(){
 			$(".text-translate li").removeClass("active");
 			$(this).addClass("active");	
 		});
+
 		// search 
 		$(".search-input").on("keyup keypress", function() {
 			if ($(this).val() != "") {
@@ -68,6 +83,7 @@ export class SidebarComponent implements OnInit {
 				$(".clear-text").addClass("d-none");
 			}
 		});
+
 		// clear text search
 		$(".clear-text").on("click", function(){
 			$(this).addClass("d-none");
