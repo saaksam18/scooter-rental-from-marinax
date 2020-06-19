376078dce3235c19c999251aa84e8022ac231150
Sreysros <sreysros@marinax.co.jp>
2020-06-19 16:47:34 +0700

    - refactory code

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,36 +1,17 @@
 $(document).ready(function () {
-	$('.pickup-slide').slick({
-		centerMode: true,
-		centerPadding: '60px',
-		slidesToShow: 3,
-		autoplay: true,
-		autoplaySpeed: 2000,
-		arrows: true,
-		prevArrow: "<img class='a-left control-c prev slick-prev' style='position: absolute;z-index: 2;height: 100%;mix-blend-mode: multiply;' src='assets/images/prev-arrow.svg'>",
-		nextArrow: "<img class='a-right control-c next slick-next' style='position: absolute;z-index: 2;right: 0;top: 0;height: 100%;mix-blend-mode: multiply;' src='assets/images/next-arrow.svg'>",
-		responsive: [
-			{
-				breakpoint: 768,
-				settings: {
-					centerPadding: '0px',
-				}
-			},
-			{
-				breakpoint: 600,
-				settings: {
-					slidesToShow: 2,
-					centerPadding: '0px',
-				}
-			},
-			{
-				breakpoint: 320,
-				settings: {
-					slidesToShow: 1,
-					centerPadding: '0px',
-				}
-			}
-		]
+	/* Loading page*/ 
+	AOS.init({
+		offset: 200,
+		duration: 600,
+		easing: 'ease-in-out',
+		delay: 100,
 	});
+	setTimeout(function () {
+		$("#wrapper").removeClass("invisible");
+		$("#wrapper").addClass("visible");
+		$("#wrapper").addClass("page-load");
+	}, 4500);
+	/* end */
 
 	$(window).on("load scroll resize", function () {
 		// About us page title animation
