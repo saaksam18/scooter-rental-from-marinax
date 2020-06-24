109ffe7531206d46a2b99533039b268bd8867200
kanha mx <kanha@marinax.co.jp>
2020-06-24 14:01:35 +0700

    added page title

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-
+import { Meta ,Title } from '@angular/platform-browser';
+import { TranslateService ,LangChangeEvent } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 @Component({
     selector: 'app-blog',
     templateUrl: './blog.component.html',
@@ -10,9 +12,26 @@ import { HttpClient } from '@angular/common/http';
 export class BlogComponent implements OnInit {
     posts: any = [];
     postsApi = environment.wpPostsApi;
+
+    title = this.translate.get('Blog').subscribe((res: string) => {
+        this.pageTitle.setTitle(res);
+    
+    });
     constructor(
+        private meta: Meta,
+        private pageTitle: Title,
+        private translate: TranslateService,
+        private router: Router,
         private http: HttpClient
-    ) { }
+      ) {
+        if(this.router.url === '/blog'){
+          translate.onLangChange.subscribe((event: LangChangeEvent) => {
+              translate.get('Blog').subscribe((res: string) => {
+              this.pageTitle.setTitle(res);
+              });
+          });
+        }
+      }
 
     ngOnInit() {
         // this.http.get(this.postsApi).subscribe(res => {
