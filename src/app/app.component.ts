d8dcc60df7f4c76555b0a0fd81274d4701a7f3c3
Sreysros <sreysros@marinax.co.jp>
2020-06-26 13:17:45 +0700

    - create button go to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component , OnInit } from '@angular/core';
+import { Component , OnInit , HostListener, ElementRef} from '@angular/core';
+import smoothscroll from 'smoothscroll-polyfill';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,27 @@ import { Component , OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   isLoading = true;
+  isShow: boolean;
+  topPosToStartShowing = 100;
 
+  @HostListener('window:scroll')
+  checkScroll() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+      smoothscroll.polyfill();
+    } else {
+      this.isShow = false;
+    }
+  }
+  // TODO: Cross browsing
+  gotoTop() {
+    window.scroll({ 
+      top: 0, 
+      left: 0, 
+      behavior: 'smooth' 
+    });
+  }
   ngOnInit() {
 		setTimeout(()=>{
       this.isLoading = false;
